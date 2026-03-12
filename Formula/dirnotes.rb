f91cc17638af8552ed6e83eecb9bb950d7a8d575
AndersT <anders.thorsrud@rubic.no>
2026-03-12 18:05:54 +0100

    bump version

--- Formula/dirnotes.rb
+++ Formula/dirnotes.rb
@@ -1,7 +1,7 @@
 class Dirnotes < Formula
   desc "A simple neovim based notes editor, where you can take notes based on your cwd"
   homepage "https://github.com/Andrrs64/homebrew-dirnotes"
-  url "https://github.com/Andrrs64/homebrew-dirnotes/archive/refs/tags/0.1.tar.gz"
+  url "https://github.com/Andrrs64/homebrew-dirnotes/archive/refs/tags/0.2.tar.gz"
   sha256 "8ae1e91c3c00d39398cd12387b45069727ba481c7aac66f0db33aabb4941bed7"
   license "MIT"
 
