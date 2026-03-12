028cc0206c0345450828584dff3ef04475995889
AndersT <anders.thorsrud@rubic.no>
2026-03-12 17:25:26 +0100

    add formula

--- Formula/dirnotes.rb
+++ Formula/dirnotes.rb
@@ -1,5 +1,18 @@
-
-class Mytool < Formula
+class DirnotesCli < Formula
   desc "A simple neovim based notes editor, where you can take notes based on your cwd"
-  homepage "https://github.com/Andrrs64/dirnotes-cli.git"
+  homepage "https://github.com/Andrrs64/dirnotes-cli"
+  url "https://github.com/Andrrs64/dirnotes-cli/archive/refs/tags/0.1.tar.gz"
+  sha256 "0701e24c8dccb8aa8988e9c6d6c75616db81b7042d1c1d4331483a96af7a6590"
+  license "MIT"
 
+  depends_on "nvim"
+
+  def install
+    bin.install "src/dirnotes.sh" => "dirnotes"
+    (share/"dirnotes").install Dir("share/dirnotes/*")
+  end
+
+  test do
+    system "#{bin}/dirnotes"
+  end
+end
