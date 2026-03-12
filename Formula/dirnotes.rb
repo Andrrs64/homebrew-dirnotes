438d8597bf336c3bf3cfd546121dad75f88a5e04
AndersT <anders.thorsrud@rubic.no>
2026-03-12 18:07:58 +0100

    update sha-256

--- Formula/dirnotes.rb
+++ Formula/dirnotes.rb
@@ -2,7 +2,7 @@ class Dirnotes < Formula
   desc "A simple neovim based notes editor, where you can take notes based on your cwd"
   homepage "https://github.com/Andrrs64/homebrew-dirnotes"
   url "https://github.com/Andrrs64/homebrew-dirnotes/archive/refs/tags/0.2.tar.gz"
-  sha256 "8ae1e91c3c00d39398cd12387b45069727ba481c7aac66f0db33aabb4941bed7"
+  sha256 "b2edc382de1fa6f3c9ad80f27f06037b51726e06301742f444a52aab97d6e502"
   license "MIT"
 
   depends_on "nvim"
